19134d0b1f7e00d883938fa45d23f275d5fb5238
DismissedLight <1686188646@qq.com>
2022-04-18 14:24:26 +0800

    init commit

---
+++ src/Snap.Hutao/Snap.Hutao/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Snap.Hutao;
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+    }
+}
