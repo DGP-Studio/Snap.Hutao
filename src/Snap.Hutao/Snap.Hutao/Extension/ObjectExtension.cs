147312c289161a1907a0662b7f69b87e25066215
Lightczx <1686188646@qq.com>
2023-07-16 21:56:07 +0800

    Update ObjectExtension.cs

--- src/Snap.Hutao/Snap.Hutao/Extension/ObjectExtension.cs
+++ src/Snap.Hutao/Snap.Hutao/Extension/ObjectExtension.cs
@@ -16,9 +16,13 @@ internal static class ObjectExtension
     /// <typeparam name="T">数据类型</typeparam>
     /// <param name="source">源</param>
     /// <returns>数组</returns>
+    [Obsolete("Use C# 12 Collection Literals")]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T[] ToArray<T>(this T source)
     {
+        // TODO: use C# 12 collection literals
+        // [ source ]
+        // and mark this as Obsolete
         return new[] { source };
     }
 }
