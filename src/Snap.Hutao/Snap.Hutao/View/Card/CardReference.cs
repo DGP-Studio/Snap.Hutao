2a757dce51b423aef97bef2dace62f91d33e49c3
DismissedLight <1686188646@qq.com>
2023-09-20 23:50:24 +0800

    UI/UX fine tuning

--- src/Snap.Hutao/Snap.Hutao/View/Card/CardReference.cs
+++ src/Snap.Hutao/Snap.Hutao/View/Card/CardReference.cs
@@ -5,6 +5,10 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace Snap.Hutao.View.Card;
 
+/// <summary>
+/// 保存对卡片的引用
+/// ItemsRepeater 无法直接使用带有 DataContext 属性的类为直接的源
+/// </summary>
 internal sealed class CardReference
 {
     public Button? Card { get; set; }
