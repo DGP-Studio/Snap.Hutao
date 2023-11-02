59f889567505623f05128bbfbc93b63c2b05b4b5
Lightczx <1686188646@qq.com>
2023-11-02 15:05:06 +0800

    MarqueeText

---
+++ src/Snap.Hutao/Snap.Hutao/Control/Behavior/MarqueeTextBehavior.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using CommunityToolkit.Labs.WinUI.MarqueeTextRns;
+using CommunityToolkit.WinUI.Behaviors;
+using Microsoft.UI.Xaml.Input;
+
+namespace Snap.Hutao.Control.Behavior;
+
+internal sealed class MarqueeTextBehavior : BehaviorBase<MarqueeText>
+{
+    private readonly PointerEventHandler pointerEnteredEventHandler;
+    private readonly PointerEventHandler pointerExitedEventHandler;
+
+    public MarqueeTextBehavior()
+    {
+        pointerEnteredEventHandler = OnPointerEntered;
+        pointerExitedEventHandler = OnPointerExited;
+    }
+
+    protected override bool Initialize()
+    {
+        AssociatedObject.PointerEntered += pointerEnteredEventHandler;
+        AssociatedObject.PointerExited += pointerExitedEventHandler;
+
+        return true;
+    }
+
+    protected override bool Uninitialize()
+    {
+        AssociatedObject.PointerEntered -= pointerEnteredEventHandler;
+        AssociatedObject.PointerExited -= pointerExitedEventHandler;
+
+        return true;
+    }
+
+    private void OnPointerEntered(object sender, PointerRoutedEventArgs e)
+    {
+        AssociatedObject.StartMarquee();
+    }
+
+    private void OnPointerExited(object sender, PointerRoutedEventArgs e)
+    {
+        AssociatedObject.StopMarquee();
+    }
+}
