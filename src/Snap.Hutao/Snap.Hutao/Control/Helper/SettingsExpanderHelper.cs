0c7ce7a72f193c99d7cf27ff7c7a328e4bf77b1b
qhy040404 <qhy040404@163.com>
2023-11-06 12:32:22 +0800

    Add files

---
+++ src/Snap.Hutao/Snap.Hutao/Control/Helper/SettingsExpanderHelper.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using CommunityToolkit.WinUI.Controls;
+using Microsoft.UI.Xaml;
+
+namespace Snap.Hutao.Control.Helper;
+
+[SuppressMessage("", "SH001")]
+[DependencyProperty("IsItemsEnabled", typeof(bool), true, nameof(OnIsItemsEnabledChanged), IsAttached = true, AttachedType = typeof(SettingsExpander))]
+public sealed partial class SettingsExpanderHelper
+{
+    private static void OnIsItemsEnabledChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+    {
+        SettingsExpander expander = (SettingsExpander)dp;
+        foreach (SettingsCard item in expander.Items.Cast<SettingsCard>())
+        {
+            item.IsEnabled = (bool)e.NewValue;
+        }
+    }
+}
