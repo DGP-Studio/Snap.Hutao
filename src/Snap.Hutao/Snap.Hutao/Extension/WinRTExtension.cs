40b055d310da48686182975e5618dfcc71b595b1
Lightczx <1686188646@qq.com>
2023-11-22 13:43:35 +0800

    lock on get winrt obj disposed

--- src/Snap.Hutao/Snap.Hutao/Extension/WinRTExtension.cs
+++ src/Snap.Hutao/Snap.Hutao/Extension/WinRTExtension.cs
@@ -10,10 +10,19 @@ internal static class WinRTExtension
 {
     public static bool IsDisposed(this IWinRTObject obj)
     {
-        return GetDisposed(obj.NativeObject);
+        IObjectReference objectReference = obj.NativeObject;
+
+        lock (GetDisposedLock(objectReference))
+        {
+            return GetDisposed(objectReference);
+        }
     }
 
     // protected bool disposed;
     [UnsafeAccessor(UnsafeAccessorKind.Field, Name ="disposed")]
     private static extern ref bool GetDisposed(IObjectReference objRef);
+
+    // private object _disposedLock
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_disposedLock")]
+    private static extern ref object GetDisposedLock(IObjectReference objRef);
 }
