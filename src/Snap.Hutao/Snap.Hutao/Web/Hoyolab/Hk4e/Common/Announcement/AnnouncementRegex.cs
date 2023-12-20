22e7942899b4a10835c340d9aa457a9c8c41294f
Lightczx <1686188646@qq.com>
2023-12-20 17:01:45 +0800

    doc

--- src/Snap.Hutao/Snap.Hutao/Web/Hoyolab/Hk4e/Common/Announcement/AnnouncementRegex.cs
+++ src/Snap.Hutao/Snap.Hutao/Web/Hoyolab/Hk4e/Common/Announcement/AnnouncementRegex.cs
@@ -7,19 +7,23 @@ namespace Snap.Hutao.Web.Hoyolab.Hk4e.Common.Announcement;
 
 internal static partial class AnnouncementRegex
 {
+    /// <inheritdoc cref="SH.WebAnnouncementMatchVersionUpdateTitle"/>
     public static readonly Regex VersionUpdateTitleRegex = new(SH.WebAnnouncementMatchVersionUpdateTitle, RegexOptions.Compiled);
 
+    /// <inheritdoc cref="SH.WebAnnouncementMatchVersionUpdateTime"/>
     public static readonly Regex VersionUpdateTimeRegex = new(SH.WebAnnouncementMatchVersionUpdateTime, RegexOptions.Compiled);
 
+    /// <inheritdoc cref="SH.WebAnnouncementMatchTransientActivityTime"/>
     public static readonly Regex TransientActivityAfterUpdateTimeRegex = new(SH.WebAnnouncementMatchTransientActivityTime, RegexOptions.Compiled);
 
+    /// <inheritdoc cref="SH.WebAnnouncementMatchPersistentActivityTime"/>
     public static readonly Regex PersistentActivityAfterUpdateTimeRegex = new(SH.WebAnnouncementMatchPersistentActivityTime, RegexOptions.Compiled);
 
+    /// <inheritdoc cref="SH.WebAnnouncementMatchPermanentActivityTime"/>
     public static readonly Regex PermanentActivityAfterUpdateTimeRegex = new(SH.WebAnnouncementMatchPermanentActivityTime, RegexOptions.Compiled);
 
-    public static readonly Regex XmlTimeTagRegex = XmlTimeTagRegexInner ??= XmlTagRegex();
-
-    private static readonly Regex? XmlTimeTagRegexInner;
+    /// <inheritdoc cref="XmlTagRegex"/>
+    public static readonly Regex XmlTimeTagRegex = XmlTagRegex();
 
     [GeneratedRegex("&lt;t class=\"t_(?:gl|lc)\".*?&gt;(.*?)&lt;/t&gt;", RegexOptions.Multiline)]
     private static partial Regex XmlTagRegex();
