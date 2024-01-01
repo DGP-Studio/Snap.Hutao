7bfea0e0906a9084ddafbe71ba097c06305c19b3
DismissedLight <1686188646@qq.com>
2024-01-01 23:20:59 +0800

    Create GameRegistryContentTest.cs

---
+++ src/Snap.Hutao/Snap.Hutao.Test/IncomingFeature/GameRegistryContentTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Text.Json;
+
+namespace Snap.Hutao.Test.IncomingFeature;
+
+[TestClass]
+public class GameRegistryContentTest
+{
+    [TestMethod]
+    [SupportedOSPlatform("windows")]
+    public void GetRegistryContent()
+    {
+        using (RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+        {
+            RegistryKey? gameKey = key.OpenSubKey(@"Software\miHoYo\原神");
+            Assert.IsNotNull(gameKey);
+
+            Dictionary<string, object> data = [];
+            foreach (string valueName in gameKey.GetValueNames())
+            {
+                data[valueName] = gameKey.GetValueKind(valueName) switch
+                {
+                    RegistryValueKind.DWord => (int)gameKey.GetValue(valueName)!,
+                    RegistryValueKind.Binary => GetString((byte[])gameKey.GetValue(valueName)!),
+                    _ => throw new NotImplementedException()
+                };
+            }
+
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            };
+
+            Console.WriteLine(JsonSerializer.Serialize(data, options));
+        }
+    }
+
+    private static unsafe string GetString(byte[] bytes)
+    {
+        fixed (byte* pByte = bytes)
+        {
+            ReadOnlySpan<byte> span = MemoryMarshal.CreateReadOnlySpanFromNullTerminated(pByte);
+            return Encoding.UTF8.GetString(span);
+        }
+    }
+}
