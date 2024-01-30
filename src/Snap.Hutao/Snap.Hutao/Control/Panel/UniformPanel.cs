431cdd12539f3ebee9772cc9b7b8aa42a287fcba
Lightczx <1686188646@qq.com>
2024-01-30 14:11:54 +0800

    refine achievement page

---
+++ src/Snap.Hutao/Snap.Hutao/Control/Panel/UniformPanel.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using CommunityToolkit.WinUI.Controls;
+
+namespace Snap.Hutao.Control.Panel;
+
+[DependencyProperty("MinItemWidth", typeof(double))]
+internal sealed partial class UniformPanel : UniformGrid
+{
+    public UniformPanel()
+    {
+        Columns = 1;
+        SizeChanged += OnSizeChanged;
+    }
+
+    private void OnSizeChanged(object sender, Microsoft.UI.Xaml.SizeChangedEventArgs e)
+    {
+        Columns = (int)((e.NewSize.Width + ColumnSpacing) / (MinItemWidth + ColumnSpacing));
+    }
+}
