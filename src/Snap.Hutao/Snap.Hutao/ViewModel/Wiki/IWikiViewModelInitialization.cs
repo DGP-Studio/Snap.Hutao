6de3cba550be704f41bffe18b375975cff5647b5
qhy040404 <qhy040404@163.com>
2024-03-04 00:09:38 +0800

    remove unused code

--- src/Snap.Hutao/Snap.Hutao/ViewModel/Wiki/IWikiViewModelInitialization.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) DGP Studio. All rights reserved.
-// Licensed under the MIT license.
-
-namespace Snap.Hutao.ViewModel.Wiki;
-
-internal interface IWikiViewModelInitialization
-{
-    void Initialize(ITokenizingTextBoxAccessor accessor);
-}
