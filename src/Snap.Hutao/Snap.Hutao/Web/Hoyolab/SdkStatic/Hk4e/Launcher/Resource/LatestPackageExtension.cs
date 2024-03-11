1a300e8b9ccecf7b1b26816c2153b28d7d9f839b
Lightczx <1686188646@qq.com>
2024-03-11 11:47:43 +0800

    fix predownload latest path and name

---
+++ src/Snap.Hutao/Snap.Hutao/Web/Hoyolab/SdkStatic/Hk4e/Launcher/Resource/LatestPackageExtension.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Text;
+
+namespace Snap.Hutao.Web.Hoyolab.SdkStatic.Hk4e.Launcher.Resource;
+
+internal static class LatestPackageExtension
+{
+    public static void Patch(this LatestPackage latest)
+    {
+        StringBuilder pathBuilder = new();
+        foreach (PackageSegment segment in latest.Segments)
+        {
+            pathBuilder.AppendLine(segment.Path);
+        }
+
+        latest.Path = pathBuilder.ToStringTrimEndReturn();
+        latest.Name = latest.Segments[0].Path[..^4]; // .00X
+    }
+}
