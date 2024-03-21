1d0a72493ed2203ccb2c55596a5b1d06348156b4
DismissedLight <1686188646@qq.com>
2024-03-21 22:27:16 +0800

    achievement city typedef

---
+++ src/Snap.Hutao/Snap.Hutao/Model/Metadata/City.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Snap.Hutao.Model.Metadata;
+
+// CityTaskOpenExcelConfig
+internal enum City : uint
+{
+    Mondstadt = 1,
+    Liyue = 2,
+    Inazuma = 3,
+    Sumeru = 4,
+    Fontaine = 5,
+    Natlan,
+    Snezhnaya,
+}
