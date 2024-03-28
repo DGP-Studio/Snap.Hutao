317b68b1ce40c5519584cb4d58396dd60ceb1127
Lightczx <1686188646@qq.com>
2024-03-28 11:18:09 +0800

    improve image cache

---
+++ src/Snap.Hutao/Snap.Hutao/Core/IO/Hashing/Hash.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Snap.Hutao.Core.IO.Hashing;
+
+internal static class Hash
+{
+    public static string SHA1HexString(string input)
+    {
+        return HashCore(BitConverter.ToString, SHA1.HashData, Encoding.UTF8.GetBytes, input);
+    }
+
+    private static TResult HashCore<TInput, TResult>(Func<byte[], TResult> resultConverter, Func<byte[], byte[]> hashMethod, Func<TInput, byte[]> bytesConverter, TInput input)
+    {
+        return resultConverter(hashMethod(bytesConverter(input)));
+    }
+}
