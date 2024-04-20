88d7f0bcc7c3cbc21adf3895f087525c15240f73
qhy040404 <qhy040404@163.com>
2024-04-20 22:22:53 +0800

    fix miss image cache

--- src/Snap.Hutao/Snap.Hutao/Core/IO/Hashing/Hash.cs
+++ src/Snap.Hutao/Snap.Hutao/Core/IO/Hashing/Hash.cs
@@ -10,7 +10,7 @@ internal static class Hash
 {
     public static string SHA1HexString(string input)
     {
-        return HashCore(BitConverter.ToString, SHA1.HashData, Encoding.UTF8.GetBytes, input);
+        return HashCore(System.Convert.ToHexString, SHA1.HashData, Encoding.UTF8.GetBytes, input);
     }
 
     private static TResult HashCore<TInput, TResult>(Func<byte[], TResult> resultConverter, Func<byte[], byte[]> hashMethod, Func<TInput, byte[]> bytesConverter, TInput input)
