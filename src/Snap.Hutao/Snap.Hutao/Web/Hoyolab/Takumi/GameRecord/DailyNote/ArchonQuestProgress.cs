1a29908e5d28dbc7f9e7915699e82daa8c4452d2
qhy040404 <qhy040404@163.com>
2024-04-20 10:57:58 +0800

    partial impl #1203

--- src/Snap.Hutao/Snap.Hutao/Web/Hoyolab/Takumi/GameRecord/DailyNote/ArchonQuestProgress.cs
+++ src/Snap.Hutao/Snap.Hutao/Web/Hoyolab/Takumi/GameRecord/DailyNote/ArchonQuestProgress.cs
@@ -5,8 +5,56 @@ namespace Snap.Hutao.Web.Hoyolab.Takumi.GameRecord.DailyNote;
 
 internal sealed class ArchonQuestProgress
 {
+    public List<uint> ArchonQuestIds { get; } = [
+        1001U, 1002U, 1003U,
+        1101U, 1102U, 1103U, 1104U,
+        1201U, 1202U, 1203U, 1204U, 1205U, 1206U, 1207U,
+        1301U, 1302U, 1303U, 1304U, 1305U, 1306U, 1307U, 1308U,
+        1401U, 1402U, 1403U, 1404U, 1405U
+    ];
+
     [JsonPropertyName("list")]
     public List<ArchonQuest> List { get; set; } = default!;
+
+    public int ArchonQuestStatusValue
+    {
+        get
+        {
+            if (List.IsNullOrEmpty())
+            {
+                return ArchonQuestIds.Count;
+            }
+
+            return ArchonQuestIds.IndexOf(List.Single().Id);
+        }
+    }
+
+    public string ArchonQuestStatusFormatted
+    {
+        get
+        {
+            if (List.IsNullOrEmpty())
+            {
+                return SH.WebDailyNoteArchonQuestStatusFinished;
+            }
+
+            return List.Single().Status.GetLocalizedDescription();
+        }
+    }
+
+    public string ArchonQuestChapterFormatted
+    {
+        get
+        {
+            if (List.IsNullOrEmpty())
+            {
+                return string.Empty;
+            }
+
+            ArchonQuest quest = List.Single();
+            return $"{quest.ChapterNum} {quest.ChapterTitle}";
+        }
+    }
 
     [JsonPropertyName("is_open_archon_quest")]
     public bool IsOpenArchonQuest { get; set; }
