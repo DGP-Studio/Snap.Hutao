a592816661d14b02b390aa96d77c889bcc7b7dc5
Lightczx <1686188646@qq.com>
2024-04-22 14:17:15 +0800

    Add quest type

---
+++ src/Snap.Hutao/Snap.Hutao/Model/Intrinsic/QuestType.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Snap.Hutao.Model.Intrinsic;
+
+internal enum QuestType
+{
+    AQ,
+    FQ,
+    LQ,
+    EQ,
+    DQ,
+    IQ,
+    VQ,
+    WQ,
+}
