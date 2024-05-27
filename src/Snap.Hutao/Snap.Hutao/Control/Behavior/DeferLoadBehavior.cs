0cc48973542a5d1927d69ddcdc13e2489625a453
DismissedLight <1686188646@qq.com>
2024-05-27 23:24:03 +0800

    deferload experiment

---
+++ src/Snap.Hutao/Snap.Hutao/Control/Behavior/DeferLoadBehavior.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using CommunityToolkit.WinUI.Behaviors;
+using Microsoft.UI.Xaml;
+using System.Runtime.InteropServices;
+
+namespace Snap.Hutao.Control.Behavior;
+
+[DependencyProperty("ElementNames", typeof(DeferLoadCollection))]
+internal sealed partial class DeferLoadBehavior : BehaviorBase<FrameworkElement>
+{
+    protected override bool Initialize()
+    {
+        if (ElementNames.IsNullOrEmpty())
+        {
+            return true;
+        }
+
+        ThreadPool.UnsafeQueueUserWorkItem(LoadElements, this);
+        return true;
+    }
+
+    private static void LoadElements(object? state)
+    {
+        if (state is not DeferLoadBehavior behavior)
+        {
+            return;
+        }
+
+        List<string>? elementNames = null;
+        behavior.AssociatedObject.DispatcherQueue.Invoke(() => elementNames = [.. behavior.ElementNames]);
+
+        foreach (string name in CollectionsMarshal.AsSpan(elementNames))
+        {
+            Thread.Sleep(1000);
+
+            behavior.AssociatedObject.DispatcherQueue.TryEnqueue(() =>
+            {
+                behavior.AssociatedObject.FindName(name);
+            });
+        }
+    }
+}
---
+++ src/Snap.Hutao/Snap.Hutao/View/Helper/LoadDeferral.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Primitives;
+using Windows.Foundation.Metadata;
+
+namespace Snap.Hutao.View.Helper;
+
+internal sealed class LoadDeferral : ObservableObject
+{
+    private bool canLoad;
+
+    public bool CanLoad { get => canLoad; set => SetProperty(ref canLoad, value); }
+}
