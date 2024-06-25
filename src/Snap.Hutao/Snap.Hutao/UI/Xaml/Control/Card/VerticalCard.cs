0075d79b0c66b2ae167af7686684f48805bd7aae
DismissedLight <1686188646@qq.com>
2024-06-25 22:39:53 +0800

    refactor

---
+++ src/Snap.Hutao/Snap.Hutao/UI/Xaml/Control/Card/VerticalCard.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.UI.Xaml;
+
+namespace Snap.Hutao.UI.Xaml.Control.Card;
+
+[DependencyProperty("Top", typeof(UIElement), default!)]
+[DependencyProperty("Bottom", typeof(UIElement), default!)]
+[DependencyProperty("BottomPadding", typeof(Thickness), default!)]
+internal sealed partial class VerticalCard : Microsoft.UI.Xaml.Controls.Control
+{
+    public VerticalCard()
+    {
+        DefaultStyleKey = typeof(VerticalCard);
+    }
+}
