0a24e196251d040fd2a8159d0dff0cb5ad19e9d5
DismissedLight <1686188646@qq.com>
2024-07-09 14:05:47 +0800

    try fix uid profilepicture duplicated

--- src/Snap.Hutao/Snap.Hutao/Core/Json/Converter/DateTimeOffsetConverter.cs
+++ src/Snap.Hutao/Snap.Hutao/Core/Json/Converter/DateTimeOffsetConverter.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 namespace Snap.Hutao.Core.Json.Converter;
 
 // 此转换器无法实现无损往返 必须在反序列化后调整 Offset
-internal class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+internal sealed class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
 {
     private const string Format = "yyyy-MM-dd HH:mm:ss";
 
