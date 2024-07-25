2e8985819fc43780cbcf6ef7d31260dcc054b954
qhy040404 <qhy040404@163.com>
2024-07-25 20:30:16 +0800

    limit memory pool size

--- src/Snap.Hutao/Snap.Hutao/Factory/IO/MemoryStreamFactory.cs
+++ src/Snap.Hutao/Snap.Hutao/Factory/IO/MemoryStreamFactory.cs
@@ -9,7 +9,21 @@ namespace Snap.Hutao.Factory.IO;
 [Injection(InjectAs.Singleton, typeof(IMemoryStreamFactory))]
 internal sealed class MemoryStreamFactory : IMemoryStreamFactory
 {
-    private readonly RecyclableMemoryStreamManager manager = new();
+    private readonly RecyclableMemoryStreamManager manager;
+
+    public MemoryStreamFactory()
+    {
+        RecyclableMemoryStreamManager.Options options = new()
+        {
+            BlockSize = 256 * 1024,
+            LargeBufferMultiple = 1024 * 1024,
+            MaximumBufferSize = 32 * 1024 * 1024,
+            MaximumSmallPoolFreeBytes = 16 * 1024 * 1024,
+            MaximumLargePoolFreeBytes = 128 * 1024 * 1024,
+        };
+
+        manager = new(options);
+    }
 
     public MemoryStream GetStream()
     {
