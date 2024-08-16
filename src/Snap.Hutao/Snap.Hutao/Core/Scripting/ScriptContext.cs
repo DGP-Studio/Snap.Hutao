4d90f6b9576a5237fbcb535123e254ea39bcdecd
DismissedLight <1686188646@qq.com>
2024-08-16 13:57:16 +0800

    refine scripting environment

--- src/Snap.Hutao/Snap.Hutao/Core/Scripting/ScriptContext.cs
+++ src/Snap.Hutao/Snap.Hutao/Core/Scripting/ScriptContext.cs
@@ -1,10 +1,103 @@
 ﻿// Copyright (c) DGP Studio. All rights reserved.
 // Licensed under the MIT license.
 
+using Snap.Hutao.Service.User;
+using Snap.Hutao.Web;
+using Snap.Hutao.Web.Hoyolab;
+using Snap.Hutao.Web.Hoyolab.DataSigning;
+using Snap.Hutao.Web.Request.Builder;
+using Snap.Hutao.Web.Request.Builder.Abstraction;
+using System.Net.Http;
+
 namespace Snap.Hutao.Core.Scripting;
 
 [SuppressMessage("", "SH001", Justification = "ScriptContext must be public in order to be exposed to the scripting environment")]
 public sealed class ScriptContext
 {
     public IServiceProvider ServiceProvider { get; } = Ioc.Default;
+
+    public static string FormatJson(string input)
+    {
+        return JsonSerializer.Serialize(JsonSerializer.Deserialize<JsonElement>(input), Core.Json.JsonOptions.Default);
+    }
+
+    public async ValueTask<string> RequestWithCurrentUserAndUidAsync(string method, string url, string[] headers, string? body, string? ds = default)
+    {
+        using (IServiceScope scope = ServiceProvider.CreateScope())
+        {
+            IUserService userService = scope.ServiceProvider.GetRequiredService<IUserService>();
+            if (await userService.GetCurrentUserAndUidAsync().ConfigureAwait(false) is not { } userAndUid)
+            {
+                return "No user and uid selected";
+            }
+
+            IHttpRequestMessageBuilderFactory httpRequestMessageBuilderFactory = scope.ServiceProvider.GetRequiredService<IHttpRequestMessageBuilderFactory>();
+            HttpRequestMessageBuilder builder = httpRequestMessageBuilderFactory.Create()
+                .SetMethod(method)
+                .SetRequestUri(url)
+                .SetUserCookieAndFpHeader(userAndUid, CookieType.All);
+
+            foreach (string header in headers)
+            {
+                int indexOfColon = header.IndexOf(':', StringComparison.Ordinal);
+                if (indexOfColon > 0)
+                {
+                    builder.AddHeader(header.AsSpan()[..indexOfColon].Trim().ToString(), header.AsSpan()[(indexOfColon + 1)..].Trim().ToString());
+                }
+                else
+                {
+                    builder.AddHeader(header);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(body))
+            {
+                builder.SetStringContent(body);
+            }
+
+            if (ParseDSFromString(ds) is { } sign)
+            {
+                await sign(builder).ConfigureAwait(false);
+            }
+
+            ILogger<ScriptContext> logger = scope.ServiceProvider.GetRequiredService<ILogger<ScriptContext>>();
+
+            using (HttpClient httpClient = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient())
+            {
+                return await SendAsync(builder, httpClient, logger, default).ConfigureAwait(false);
+            }
+        }
+    }
+
+    private static Func<HttpRequestMessageBuilder, ValueTask>? ParseDSFromString(string? instruction)
+    {
+        if (string.IsNullOrEmpty(instruction))
+        {
+            return default;
+        }
+
+        string[] parts = instruction.Split('|');
+        DataSignAlgorithmVersion version = Enum.Parse<DataSignAlgorithmVersion>(parts[0]);
+        SaltType saltType = Enum.Parse<SaltType>(parts[1]);
+        bool includeChars = bool.Parse(parts[2]);
+
+        return (builder) => builder.SignDataAsync(version, saltType, includeChars);
+    }
+
+    private static async ValueTask<string> SendAsync(HttpRequestMessageBuilder builder, HttpClient httpClient, ILogger logger, CancellationToken token)
+    {
+        HttpContext context = new()
+        {
+            HttpClient = httpClient,
+            Logger = logger,
+            RequestAborted = token,
+        };
+
+        using (context)
+        {
+            await builder.SendAsync(context).ConfigureAwait(false);
+            ArgumentNullException.ThrowIfNull(context.Response);
+            return await context.Response.Content.ReadAsStringAsync();
+        }
+    }
 }
--- src/Snap.Hutao/Snap.Hutao/UI/Xaml/View/Window/ScriptingWindow.xaml.cs
+++ src/Snap.Hutao/Snap.Hutao/UI/Xaml/View/Window/ScriptingWindow.xaml.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license.
 
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Media;
 using Snap.Hutao.UI.Windowing.Abstraction;
 using Snap.Hutao.ViewModel.Scripting;
 using Windows.Graphics;
@@ -14,9 +16,70 @@ internal sealed partial class ScriptingWindow : Microsoft.UI.Xaml.Window, IXamlW
     private const int MinWidth = 600;
     private const int MinHeight = 200;
 
+#pragma warning disable SA1310
+    private static readonly SolidColorBrush Brush_Interface = new(ColorHelper.ToColor(0xFFB8D7A3));
+    private static readonly SolidColorBrush Brush_Struct = new(ColorHelper.ToColor(0xFF86C691));
+    private static readonly SolidColorBrush Brush_Keyword = new(ColorHelper.ToColor(0xFF569CD6));
+    private static readonly SolidColorBrush Brush_Bracket = new(ColorHelper.ToColor(0xFF65ADE5));
+    private static readonly SolidColorBrush Brush_Method = new(ColorHelper.ToColor(0xFFDCDCAA));
+    private static readonly SolidColorBrush Brush_Argument = new(ColorHelper.ToColor(0xFF9CDCFE));
+#pragma warning restore SA1310
+
     public ScriptingWindow(IServiceProvider serviceProvider)
     {
         InitializeComponent();
+
+        {
+            InlineCollection inlines;
+
+            // Write XAML is not fesible, so we use code
+            // System.IServiceProvider ServiceProvider { get; }
+            inlines = TextBlock_ServiceProvider.Inlines;
+            inlines.Add(new Run() { Text = "System." });
+            inlines.Add(new Run() { Text = "IServiceProvider", Foreground = Brush_Interface });
+            inlines.Add(new Run() { Text = " ServiceProvider " });
+            inlines.Add(new Run() { Text = "{ ", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = "get", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "; " });
+            inlines.Add(new Run() { Text = "}", Foreground = Brush_Bracket });
+
+            // static string FormatJson(string input);
+            inlines = TextBlock_FormatJson.Inlines;
+            inlines.Add(new Run() { Text = "static string", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = " FormatJson", Foreground = Brush_Method });
+            inlines.Add(new Run() { Text = "(", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = "string", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = " input", Foreground = Brush_Argument });
+            inlines.Add(new Run() { Text = ")", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = ";" });
+
+            // System.Threading.Tasks.ValueTask<string> RequestWithCurrentUserAndUidAsync(string method, string url, string[] headers, string? body, string? ds = default)
+            inlines = TextBlock_RequestWithCurrentUserAndUidAsync.Inlines;
+            inlines.Add(new Run() { Text = "System.Threading.Tasks." });
+            inlines.Add(new Run() { Text = "ValueTask", Foreground = Brush_Struct });
+            inlines.Add(new Run() { Text = "<", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = "string", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "> ", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = "RequestWithCurrentUserAndUidAsync", Foreground = Brush_Method });
+            inlines.Add(new Run() { Text = "(", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = "string ", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "method, ", Foreground = Brush_Argument });
+            inlines.Add(new Run() { Text = "string ", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "url, ", Foreground = Brush_Argument });
+            inlines.Add(new Run() { Text = "string", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "[] ", Foreground = Brush_Bracket });
+            inlines.Add(new Run() { Text = "headers, ", Foreground = Brush_Argument });
+            inlines.Add(new Run() { Text = "string", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "? ", });
+            inlines.Add(new Run() { Text = "body, ", Foreground = Brush_Argument });
+            inlines.Add(new Run() { Text = "string", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = "? ", });
+            inlines.Add(new Run() { Text = "ds ", Foreground = Brush_Argument });
+            inlines.Add(new Run() { Text = "= " });
+            inlines.Add(new Run() { Text = "default", Foreground = Brush_Keyword });
+            inlines.Add(new Run() { Text = ")", Foreground = Brush_Bracket });
+        }
+
         RootGrid.InitializeDataContext<ScriptingViewModel>(serviceProvider);
         this.InitializeController(serviceProvider);
     }
