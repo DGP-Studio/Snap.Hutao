b9e4df65f4766df5e9b82e355ad6a51a9167e209
qhy040404 <qhy040404@163.com>
2024-10-10 09:32:07 +0800

    stop if user and server not match

--- src/Snap.Hutao/Snap.Hutao/Service/Game/Launching/Handler/LaunchExecutionSetGameAccountHandler.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Game/Launching/Handler/LaunchExecutionSetGameAccountHandler.cs
@@ -2,8 +2,9 @@
 // Licensed under the MIT license.
 
 using Snap.Hutao.Core.DependencyInjection.Abstraction;
+using Snap.Hutao.Model.Entity.Primitive;
 using Snap.Hutao.Service.Game.Account;
-using Snap.Hutao.Service.Notification;
+using Snap.Hutao.Service.Game.Scheme;
 using Snap.Hutao.Web.Hoyolab.Passport;
 using Snap.Hutao.Web.Response;
 
@@ -15,28 +16,9 @@ internal sealed class LaunchExecutionSetGameAccountHandler : ILaunchExecutionDel
     {
         if (context.Options.IsUseMiYouSheAccount)
         {
-            if (context.UserAndUid is { } userAndUid)
+            if (!await HandleMiYouSheAccountAsync(context).ConfigureAwait(false))
             {
-                Response<AuthTicketWrapper> resp;
-                using (IServiceScope scope = context.ServiceProvider.CreateScope())
-                {
-                    IHoyoPlayPassportClient client = scope.ServiceProvider
-                        .GetRequiredService<IOverseaSupportFactory<IHoyoPlayPassportClient>>()
-                        .CreateFor(userAndUid);
-                    resp = await client
-                        .CreateAuthTicketAsync(userAndUid.User, context.CancellationToken)
-                        .ConfigureAwait(false);
-                }
-
-                if (resp.IsOk())
-                {
-                    context.AuthTicket = resp.Data.Ticket;
-                }
-            }
-            else
-            {
-                IInfoBarService infoBarService = context.ServiceProvider.GetRequiredService<IInfoBarService>();
-                infoBarService.Warning("未选中米游社用户，将保留游戏内登录态");
+                return;
             }
         }
         else
@@ -55,5 +37,48 @@ internal sealed class LaunchExecutionSetGameAccountHandler : ILaunchExecutionDel
         }
 
         await next().ConfigureAwait(false);
+    }
+
+    private static async ValueTask<bool> HandleMiYouSheAccountAsync(LaunchExecutionContext context)
+    {
+        if (context.Scheme.GetSchemeType() is SchemeType.ChineseBilibili)
+        {
+            context.Logger.LogWarning("Bilibili server does not support auth ticket login");
+            return true;
+        }
+
+        if (context.UserAndUid is not { } userAndUid)
+        {
+            context.Logger.LogWarning("No user and uid selected, keep game account in game");
+            return true;
+        }
+
+        if (userAndUid.IsOversea != context.Scheme.IsOversea)
+        {
+            context.Result.Kind = LaunchExecutionResultKind.GameAccountUserAndUidAndServerNotMatch;
+            context.Result.ErrorMessage = "当前选中用户和游戏服务器不匹配，请重新选择";
+            return false;
+        }
+
+        Response<AuthTicketWrapper> resp;
+        using (IServiceScope scope = context.ServiceProvider.CreateScope())
+        {
+            IHoyoPlayPassportClient client = scope.ServiceProvider
+                .GetRequiredService<IOverseaSupportFactory<IHoyoPlayPassportClient>>()
+                .CreateFor(userAndUid);
+            resp = await client
+                .CreateAuthTicketAsync(userAndUid.User, context.CancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        if (resp.IsOk())
+        {
+            context.AuthTicket = resp.Data.Ticket;
+            return true;
+        }
+
+        context.Result.Kind = LaunchExecutionResultKind.GameAccountCreateAuthTicketFailed;
+        context.Result.ErrorMessage = "使用米游社账号登录失败，请重新登录米哈游账号";
+        return false;
     }
 }
