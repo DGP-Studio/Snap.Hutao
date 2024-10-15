d47710a634d1c16075715376558362320251365a
qhy040404 <qhy040404@163.com>
2024-10-15 21:16:54 +0800

    add third party logos

---
+++ src/Snap.Hutao/Snap.Hutao/UI/Xaml/Data/Converter/Specialized/ThirdPartyIconConverter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Snap.Hutao.UI.Xaml.Data.Converter.Specialized;
+
+internal sealed partial class ThirdPartyIconConverter : ValueConverter<string, BitmapIcon>
+{
+    public override BitmapIcon Convert(string from)
+    {
+        Uri uri = $"ms-appx:///Resource/ThirdParty/{from}.png".ToUri();
+        return new()
+        {
+            ShowAsMonochrome = false,
+            UriSource = uri,
+        };
+    }
+}
