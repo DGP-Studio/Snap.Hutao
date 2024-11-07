686fc9691fc6492a2a682484b1d2a0d112704818
DismissedLight <1686188646@qq.com>
2024-11-07 13:32:04 +0800

    Update PipeRequest.cs

--- src/Snap.Hutao/Snap.Hutao/Core/LifeCycle/InterProcess/BetterGenshinImpact/PipeRequest.cs
+++ src/Snap.Hutao/Snap.Hutao/Core/LifeCycle/InterProcess/BetterGenshinImpact/PipeRequest.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) DGP Studio. All rights reserved.
 // Licensed under the MIT license.
 
-using Snap.Hutao.Win32.Foundation;
-using System.IO.Pipes;
-
 namespace Snap.Hutao.Core.LifeCycle.InterProcess.BetterGenshinImpact;
 
 internal sealed class PipeRequest<T>
