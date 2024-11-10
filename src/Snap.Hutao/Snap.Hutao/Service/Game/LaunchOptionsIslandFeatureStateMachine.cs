85a01111f035641a913f20af9d484c54e73b7cdc
DismissedLight <1686188646@qq.com>
2024-11-10 18:42:25 +0800

    Update LaunchOptionsIslandFeatureStateMachine.cs

--- src/Snap.Hutao/Snap.Hutao/Service/Game/LaunchOptionsIslandFeatureStateMachine.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Game/LaunchOptionsIslandFeatureStateMachine.cs
@@ -7,6 +7,9 @@ namespace Snap.Hutao.Service.Game;
 
 internal sealed partial class LaunchOptionsIslandFeatureStateMachine : ObservableObject
 {
+    private const bool T = true;
+    private const bool F = false;
+
     private bool canInputTargetFov;
     private bool canToggleSetFovHotSwitch;
     private bool canToggleSetFovColdSwitch;
@@ -58,26 +61,26 @@ internal sealed partial class LaunchOptionsIslandFeatureStateMachine : Observabl
             CanToggleSetFpsHotSwitch) =
             (options.IsIslandEnabled, options.IsGameRunning, options.HookingSetFieldOfView, options.IsSetFieldOfViewEnabled, options.HookingOpenTeam, options.IsSetTargetFrameRateEnabled) switch
             {
-                (false, _, _, _, _, _) => (false, false, false, false, false, false, false, false, false, false),
-                (true, false, false, _, false, true) => (false, false, true, false, false, false, true, true, true, true),
-                (true, false, false, _, true, true) => (false, false, true, false, false, true, true, true, true, true),
-                (true, false, false, _, false, false) => (false, false, true, false, false, false, true, true, false, true),
-                (true, false, false, _, true, false) => (false, false, true, false, false, true, true, true, false, true),
-                (true, false, true, false, false, _) => (false, true, true, false, false, false, true, true, false, false),
-                (true, false, true, false, true, _) => (false, true, true, false, false, true, true, true, false, false),
-                (true, false, true, true, false, false) => (true, true, true, true, true, false, true, true, false, true),
-                (true, false, true, true, false, true) => (true, true, true, true, true, false, true, true, true, true),
-                (true, false, true, true, true, false) => (true, true, true, true, true, true, true, true, false, true),
-                (true, false, true, true, true, true) => (true, true, true, true, true, true, true, true, true, true),
-                (true, true, false, _, false, false) => (false, false, false, false, false, false, false, false, false, true),
-                (true, true, false, _, false, true) => (false, false, false, false, false, false, false, false, true, true),
-                (true, true, false, _, true, false) => (false, false, false, false, false, true, false, false, false, true),
-                (true, true, false, _, true, true) => (false, false, false, false, false, true, false, false, true, true),
-                (true, true, true, false, false, _) => (false, true, false, false, false, false, false, false, false, false),
-                (true, true, true, false, true, _) => (false, true, false, false, false, true, false, false, false, false),
-                (true, true, true, true, false, _) => (true, true, false, true, true, false, false, false, false, true),
-                (true, true, true, true, true, false) => (true, true, false, true, true, true, false, false, false, true),
-                (true, true, true, true, true, true) => (true, true, false, true, true, true, false, false, true, true),
+                (F, _, _, _, _, _) => (F, F, F, F, F, F, F, F, F, F),
+                (T, F, F, _, F, T) => (F, F, T, F, F, F, T, T, T, T),
+                (T, F, F, _, T, T) => (F, F, T, F, F, T, T, T, T, T),
+                (T, F, F, _, F, F) => (F, F, T, F, F, F, T, T, F, T),
+                (T, F, F, _, T, F) => (F, F, T, F, F, T, T, T, F, T),
+                (T, F, T, F, F, _) => (F, T, T, F, F, F, T, T, F, F),
+                (T, F, T, F, T, _) => (F, T, T, F, F, T, T, T, F, F),
+                (T, F, T, T, F, F) => (T, T, T, T, T, F, T, T, F, T),
+                (T, F, T, T, F, T) => (T, T, T, T, T, F, T, T, T, T),
+                (T, F, T, T, T, F) => (T, T, T, T, T, T, T, T, F, T),
+                (T, F, T, T, T, T) => (T, T, T, T, T, T, T, T, T, T),
+                (T, T, F, _, F, F) => (F, F, F, F, F, F, F, F, F, T),
+                (T, T, F, _, F, T) => (F, F, F, F, F, F, F, F, T, T),
+                (T, T, F, _, T, F) => (F, F, F, F, F, T, F, F, F, T),
+                (T, T, F, _, T, T) => (F, F, F, F, F, T, F, F, T, T),
+                (T, T, T, F, F, _) => (F, T, F, F, F, F, F, F, F, F),
+                (T, T, T, F, T, _) => (F, T, F, F, F, T, F, F, F, F),
+                (T, T, T, T, F, _) => (T, T, F, T, T, F, F, F, F, T),
+                (T, T, T, T, T, F) => (T, T, F, T, T, T, F, F, F, T),
+                (T, T, T, T, T, T) => (T, T, F, T, T, T, F, F, T, T),
             };
     }
 }
