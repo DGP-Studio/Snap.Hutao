2776c04e9d7d3314b63c730b9f78b56314ac8a2f
qhy040404 <qhy040404@163.com>
2024-11-16 22:23:44 +0800

    fix #2159

--- src/Snap.Hutao/Snap.Hutao/Migrations/20241116142009_RemoveUnusedInventories.Designer.cs
+++ src/Snap.Hutao/Snap.Hutao/Migrations/20241116142009_RemoveUnusedInventories.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Snap.Hutao.Model.Entity.Database;
 
@@ -10,9 +11,11 @@ using Snap.Hutao.Model.Entity.Database;
 namespace Snap.Hutao.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241116142009_RemoveUnusedInventories")]
+    partial class RemoveUnusedInventories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
