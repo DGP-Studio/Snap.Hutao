dd3aba1a137a3d4e0596561bc9a489976bf3dcd8
qhy040404 <qhy040404@163.com>
2024-11-30 16:22:40 +0800

    fix flyout wrong data

---
+++ src/Snap.Hutao/Snap.Hutao/UI/Xaml/View/Specialized/ResinStatisticsItemView.xaml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.UI.Xaml.Controls;
+using Snap.Hutao.ViewModel.Cultivation;
+
+namespace Snap.Hutao.UI.Xaml.View.Specialized;
+
+[DependencyProperty("Item", typeof(ResinStatisticsItem))]
+internal sealed partial class ResinStatisticsItemView : UserControl
+{
+    public ResinStatisticsItemView()
+    {
+        InitializeComponent();
+    }
+}
