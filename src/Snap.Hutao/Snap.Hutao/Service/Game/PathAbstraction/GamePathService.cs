52b9ac35f18a1dfc98d18ac0d59b1ee3830fb6cb
DismissedLight <1686188646@qq.com>
2024-12-09 17:25:41 +0800

    game path hot path

--- src/Snap.Hutao/Snap.Hutao/Service/Game/PathAbstraction/GamePathService.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Game/PathAbstraction/GamePathService.cs
@@ -14,30 +14,26 @@ internal sealed partial class GamePathService : IGamePathService
 
     public async ValueTask<ValueResult<bool, string>> SilentGetGamePathAsync()
     {
-        // Cannot find in setting
-        if (string.IsNullOrEmpty(launchOptions.GamePath))
+        // Found in setting
+        if (!string.IsNullOrEmpty(launchOptions.GamePath))
         {
-            // Try to locate by unity log
-            if (await gameLocatorFactory.LocateAsync(GameLocationSource.UnityLog).ConfigureAwait(false) is (true, { } path1))
-            {
-                launchOptions.UpdateGamePath(path1);
-                return new(true, launchOptions.GamePath);
-            }
-
-            // Try to locate by registry
-            if (await gameLocatorFactory.LocateAsync(GameLocationSource.Registry).ConfigureAwait(false) is (true, { } path2))
-            {
-                launchOptions.UpdateGamePath(path2);
-                return new(true, launchOptions.GamePath);
-            }
-
-            // If it's still null or empty
-            if (string.IsNullOrEmpty(launchOptions.GamePath))
-            {
-                return new(false, SH.ServiceGamePathLocateFailed);
-            }
+            return new(true, launchOptions.GamePath);
         }
 
-        return new(true, launchOptions.GamePath);
+        // Try to locate by unity log
+        if (await gameLocatorFactory.LocateAsync(GameLocationSource.UnityLog).ConfigureAwait(false) is (true, { } path1))
+        {
+            launchOptions.UpdateGamePath(path1);
+            return new(true, launchOptions.GamePath);
+        }
+
+        // Try to locate by registry
+        if (await gameLocatorFactory.LocateAsync(GameLocationSource.Registry).ConfigureAwait(false) is (true, { } path2))
+        {
+            launchOptions.UpdateGamePath(path2);
+            return new(true, launchOptions.GamePath);
+        }
+
+        return new(false, SH.ServiceGamePathLocateFailed);
     }
 }
