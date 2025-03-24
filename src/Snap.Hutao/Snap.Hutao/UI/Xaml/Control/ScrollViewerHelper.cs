90c0de1e982d8ffcbc927216dc7f7f5efc837a24
qhy040404 <qhy040404@163.com>
2025-03-24 20:01:53 +0800

    impl #1731

--- src/Snap.Hutao/Snap.Hutao/UI/Xaml/Control/ScrollViewerHelper.cs
+++ src/Snap.Hutao/Snap.Hutao/UI/Xaml/Control/ScrollViewerHelper.cs
@@ -8,4 +8,20 @@ namespace Snap.Hutao.UI.Xaml.Control;
 
 [SuppressMessage("", "SH001")]
 [DependencyProperty("RightPanel", typeof(UIElement), IsAttached = true, AttachedType = typeof(ScrollViewer))]
-public sealed partial class ScrollViewerHelper;
+[DependencyProperty("ScrollToTopAssociatedObject", typeof(object), default, nameof(OnScrollToTopAssociatedObjectChanged), IsAttached = true, AttachedType = typeof(ScrollViewer))]
+public sealed partial class ScrollViewerHelper
+{
+    private static void OnScrollToTopAssociatedObjectChanged(DependencyObject dp, DependencyPropertyChangedEventArgs args)
+    {
+        ScrollViewer scrollViewer = (ScrollViewer)dp;
+        if (!scrollViewer.IsLoaded)
+        {
+            return;
+        }
+
+        if (args.NewValue is not null)
+        {
+            scrollViewer.ChangeView(null, 0, null);
+        }
+    }
+}
