1414bcf4f7131569447149a56ac02314ff17f5d2
DismissedLight <1686188646@qq.com>
2025-04-14 15:22:30 +0800

    bootstrap

--- src/Snap.Hutao/Snap.Hutao/Bootstrap.cs
+++ src/Snap.Hutao/Snap.Hutao/Bootstrap.cs
@@ -35,6 +35,7 @@ public static partial class Bootstrap
     [ModuleInitializer]
     internal static void InitializeModule()
     {
+        // UndockedRegFreeWinRT
         // Set base directory env var for PublishSingleFile support (referenced by SxS redirection)
         Environment.SetEnvironmentVariable("MICROSOFT_WINDOWSAPPRUNTIME_BASE_DIRECTORY", AppContext.BaseDirectory);
 
@@ -47,9 +48,6 @@ public static partial class Bootstrap
 
     [LibraryImport("Microsoft.WindowsAppRuntime.dll", EntryPoint = "WindowsAppRuntime_EnsureIsLoaded")]
     private static partial int WindowsAppRuntimeEnsureIsLoaded();
-
-    [LibraryImport("Microsoft.ui.xaml.dll")]
-    private static partial void XamlCheckProcessRequirements();
 
     [STAThread]
     private static void Main(string[] args)
@@ -83,8 +81,6 @@ public static partial class Bootstrap
             Environment.SetEnvironmentVariable("WEBVIEW2_DEFAULT_BACKGROUND_COLOR", "00000000");
             Environment.SetEnvironmentVariable("DOTNET_SYSTEM_BUFFERS_SHAREDARRAYPOOL_MAXARRAYSPERPARTITION", "128");
 
-            Debug.WriteLine($"[Arguments]:[{args.ToString(',')}]");
-            XamlCheckProcessRequirements();
             ComWrappersSupport.InitializeComWrappers();
 
             // By adding the using statement, we can dispose the injected services when closing
@@ -121,7 +117,7 @@ public static partial class Bootstrap
         {
             const string Message = """
                 Snap Hutao 无法在版本低于 10.0.19045.5371 的 Windows 上运行，请更新系统。
-                Snap Hutao cannot run on Windows versions lower than 10.0.19045.5371. Please update your system.
+                Snap Hutao cannot run on Windows versions earlier than 10.0.19045.5371. Please update your system.
                 """;
             User32.MessageBoxExW(
                 default,
