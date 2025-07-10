34e7729d631851e915f7cc098f5541e20136000c
DismissedLight <1686188646@qq.com>
2025-07-10 14:56:06 +0800

    add teaching tip for some island features

---
+++ src/Snap.Hutao/Snap.Hutao/UI/Xaml/Behavior/Action/ShowTeachingTipAction.cs
@@ -0,0 +1,27 @@
+// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+
+namespace Snap.Hutao.UI.Xaml.Behavior.Action;
+
+[DependencyProperty("TeachingTip", typeof(TeachingTip))]
+internal sealed partial class ShowTeachingTipAction : DependencyObject, IAction
+{
+    public object? Execute(object? sender, object parameter)
+    {
+        if (sender is null)
+        {
+            return default;
+        }
+
+        if (TeachingTip is { } teachingTip)
+        {
+            teachingTip.IsOpen = true;
+        }
+
+        return default;
+    }
+}
