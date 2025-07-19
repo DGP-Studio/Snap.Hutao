1bd71b33e52e962e57c9ade52a169f6ceb612d3f
DismissedLight <1686188646@qq.com>
2025-07-19 10:26:00 +0800

    optimize avatar level experience calc

--- src/Snap.Hutao/Snap.Hutao/Service/Cultivation/Offline/AvatarLevelExperience.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Cultivation/Offline/AvatarLevelExperience.cs
@@ -1,129 +1,40 @@
 // Copyright (c) DGP Studio. All rights reserved.
 // Licensed under the MIT license.
 
-using System.Collections.Frozen;
-
 namespace Snap.Hutao.Service.Cultivation.Offline;
 
 internal static class AvatarLevelExperience
 {
-    private const int MaxLevel = 90;
-
-    private static readonly FrozenDictionary<int, int> LevelExperienceData = FrozenDictionary.ToFrozenDictionary(
-    [
-        KeyValuePair.Create(1, 1000),
-        KeyValuePair.Create(2, 1325),
-        KeyValuePair.Create(3, 1700),
-        KeyValuePair.Create(4, 2150),
-        KeyValuePair.Create(5, 2625),
-        KeyValuePair.Create(6, 3150),
-        KeyValuePair.Create(7, 3725),
-        KeyValuePair.Create(8, 4350),
-        KeyValuePair.Create(9, 5000),
-        KeyValuePair.Create(10, 5700),
-        KeyValuePair.Create(11, 6450),
-        KeyValuePair.Create(12, 7225),
-        KeyValuePair.Create(13, 8050),
-        KeyValuePair.Create(14, 8925),
-        KeyValuePair.Create(15, 9825),
-        KeyValuePair.Create(16, 10750),
-        KeyValuePair.Create(17, 11725),
-        KeyValuePair.Create(18, 12725),
-        KeyValuePair.Create(19, 13775),
-        KeyValuePair.Create(20, 14875),
-        KeyValuePair.Create(21, 16800),
-        KeyValuePair.Create(22, 18000),
-        KeyValuePair.Create(23, 19250),
-        KeyValuePair.Create(24, 20550),
-        KeyValuePair.Create(25, 21875),
-        KeyValuePair.Create(26, 23250),
-        KeyValuePair.Create(27, 24650),
-        KeyValuePair.Create(28, 26100),
-        KeyValuePair.Create(29, 27575),
-        KeyValuePair.Create(30, 29100),
-        KeyValuePair.Create(31, 30650),
-        KeyValuePair.Create(32, 32250),
-        KeyValuePair.Create(33, 33875),
-        KeyValuePair.Create(34, 35550),
-        KeyValuePair.Create(35, 37250),
-        KeyValuePair.Create(36, 38975),
-        KeyValuePair.Create(37, 40750),
-        KeyValuePair.Create(38, 42575),
-        KeyValuePair.Create(39, 44425),
-        KeyValuePair.Create(40, 46300),
-        KeyValuePair.Create(41, 50625),
-        KeyValuePair.Create(42, 52700),
-        KeyValuePair.Create(43, 54775),
-        KeyValuePair.Create(44, 56900),
-        KeyValuePair.Create(45, 59075),
-        KeyValuePair.Create(46, 61275),
-        KeyValuePair.Create(47, 63525),
-        KeyValuePair.Create(48, 65800),
-        KeyValuePair.Create(49, 68125),
-        KeyValuePair.Create(50, 70475),
-        KeyValuePair.Create(51, 76500),
-        KeyValuePair.Create(52, 79050),
-        KeyValuePair.Create(53, 81650),
-        KeyValuePair.Create(54, 84275),
-        KeyValuePair.Create(55, 86950),
-        KeyValuePair.Create(56, 89650),
-        KeyValuePair.Create(57, 92400),
-        KeyValuePair.Create(58, 95175),
-        KeyValuePair.Create(59, 98000),
-        KeyValuePair.Create(60, 100875),
-        KeyValuePair.Create(61, 108950),
-        KeyValuePair.Create(62, 112050),
-        KeyValuePair.Create(63, 115175),
-        KeyValuePair.Create(64, 118325),
-        KeyValuePair.Create(65, 121525),
-        KeyValuePair.Create(66, 124775),
-        KeyValuePair.Create(67, 128075),
-        KeyValuePair.Create(68, 131400),
-        KeyValuePair.Create(69, 134775),
-        KeyValuePair.Create(70, 138175),
-        KeyValuePair.Create(71, 148700),
-        KeyValuePair.Create(72, 152375),
-        KeyValuePair.Create(73, 156075),
-        KeyValuePair.Create(74, 159825),
-        KeyValuePair.Create(75, 163600),
-        KeyValuePair.Create(76, 167425),
-        KeyValuePair.Create(77, 171300),
-        KeyValuePair.Create(78, 175225),
-        KeyValuePair.Create(79, 179175),
-        KeyValuePair.Create(80, 183175),
-        KeyValuePair.Create(81, 216225),
-        KeyValuePair.Create(82, 243025),
-        KeyValuePair.Create(83, 273100),
-        KeyValuePair.Create(84, 306800),
-        KeyValuePair.Create(85, 344600),
-        KeyValuePair.Create(86, 386950),
-        KeyValuePair.Create(87, 434425),
-        KeyValuePair.Create(88, 487625),
-        KeyValuePair.Create(89, 547200),
-        KeyValuePair.Create(90, 0), // Level 90 does not require experience to level up, as it is the maximum level.
-    ]);
-
-    public static int GetExperienceToNextLevel(int level)
+    // AvatarLevelExcelConfigData
+    private static ReadOnlySpan<int> LevelExperienceData
     {
-        if (level >= MaxLevel)
-        {
-            return 0;
-        }
-
-        return LevelExperienceData.TryGetValue(level, out int experience) ? experience : 0;
+        get =>
+        [
+            000000, // Level 0 for padding
+            001000, 001325, 001700, 002150, 002625, 003150, 003725, 004350, 005000, 005700,
+            006450, 007225, 008050, 008925, 009825, 010750, 011725, 012725, 013775, 014875,
+            016800, 018000, 019250, 020550, 021875, 023250, 024650, 026100, 027575, 029100,
+            030650, 032250, 033875, 035550, 037250, 038975, 040750, 042575, 044425, 046300,
+            050625, 052700, 054775, 056900, 059075, 061275, 063525, 065800, 068125, 070475,
+            076500, 079050, 081650, 084275, 086950, 089650, 092400, 095175, 098000, 100875,
+            108950, 112050, 115175, 118325, 121525, 124775, 128075, 131400, 134775, 138175,
+            148700, 152375, 156075, 159825, 163600, 167425, 171300, 175225, 179175, 183175,
+            216225, 243025, 273100, 306800, 344600, 386950, 434425, 487625, 547200, 000000
+        ];
     }
 
     public static int CalculateTotalExperience(int currentLevel, int targetLevel)
     {
-        if (currentLevel >= targetLevel || currentLevel >= MaxLevel)
+        // Generally, these conditions should not be reached, but they are here for span safety.
+        if (currentLevel >= targetLevel || targetLevel > 90 || currentLevel < 1)
         {
             return 0;
         }
 
         int totalExperience = 0;
-        for (int level = currentLevel; level < targetLevel && level < MaxLevel; level++)
+        foreach (ref readonly int exp in LevelExperienceData[currentLevel..targetLevel])
         {
-            totalExperience += GetExperienceToNextLevel(level);
+            totalExperience += exp;
         }
 
         return totalExperience;
