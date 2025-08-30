ca72011d8d8254828510151444c3678ea9d5369c
DismissedLight <1686188646@qq.com>
2025-08-30 19:26:32 +0800

    bump native

--- src/Snap.Hutao/Snap.Hutao/Service/Game/Island/DllInjectionUtilities.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Game/Island/DllInjectionUtilities.cs
@@ -31,13 +31,21 @@ internal static class DllInjectionUtilities
         }
     }
 
+    public static unsafe void InjectUsingRemoteThread(ReadOnlySpan<char> dllPath, int processId)
+    {
+        fixed (char* pDllPath = dllPath)
+        {
+            Marshal.ThrowExceptionForHR(DllInjectionUtilitiesInjectUsingRemoteThread(pDllPath, processId));
+        }
+    }
+
     public static unsafe void InjectUsingRemoteThread(ReadOnlySpan<char> dllPath, ReadOnlySpan<char> functionName, int processId)
     {
         fixed (char* pDllPath = dllPath)
         {
             fixed (char* pFunctionName = functionName)
             {
-                Marshal.ThrowExceptionForHR(DllInjectionUtilitiesInjectUsingRemoteThread(pDllPath, pFunctionName, processId));
+                Marshal.ThrowExceptionForHR(DllInjectionUtilitiesInjectUsingRemoteThreadWithFunction(pDllPath, pFunctionName, processId));
             }
         }
     }
@@ -52,5 +60,8 @@ internal static class DllInjectionUtilities
     private static extern HRESULT DllInjectionUtilitiesInjectUsingWindowsHook2(PCWSTR dllPath, PCWSTR functionName, int processId);
 
     [DllImport(HutaoNativeMethods.DllName, CallingConvention = CallingConvention.Winapi, ExactSpelling = true)]
-    private static extern HRESULT DllInjectionUtilitiesInjectUsingRemoteThread(PCWSTR dllPath, PCWSTR functionName, int processId);
+    private static extern HRESULT DllInjectionUtilitiesInjectUsingRemoteThread(PCWSTR dllPath, int processId);
+
+    [DllImport(HutaoNativeMethods.DllName, CallingConvention = CallingConvention.Winapi, ExactSpelling = true)]
+    private static extern HRESULT DllInjectionUtilitiesInjectUsingRemoteThreadWithFunction(PCWSTR dllPath, PCWSTR functionName, int processId);
 }
