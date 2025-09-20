f74b9ea1f8af6da19f876c7c915c22ff8c161263
DismissedLight <1686188646@qq.com>
2025-09-20 15:11:48 +0800

    reward taken control

--- src/Snap.Hutao/Snap.Hutao/Service/Game/Launching/Handler/LaunchExecutionGameProcessStartHandler.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Game/Launching/Handler/LaunchExecutionGameProcessStartHandler.cs
@@ -8,24 +8,24 @@ namespace Snap.Hutao.Service.Game.Launching.Handler;
 
 internal sealed class LaunchExecutionGameProcessStartHandler : AbstractLaunchExecutionHandler
 {
-    public override ValueTask ExecuteAsync(LaunchExecutionContext context)
+    public override async ValueTask ExecuteAsync(LaunchExecutionContext context)
     {
         try
         {
             context.Process.Start();
+            await context.TaskContext.SwitchToMainThreadAsync();
             GameLifeCycle.IsGameRunningProperty.Value = true;
         }
         catch (Win32Exception ex)
         {
             if (ex.HResult is HRESULT.E_FAIL)
             {
-                return ValueTask.CompletedTask;
+                return;
             }
 
             throw;
         }
 
         context.Progress.Report(new(SH.ServiceGameLaunchPhaseProcessStarted));
-        return ValueTask.CompletedTask;
     }
 }
