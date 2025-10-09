64791f663f7f8bbe1264e9cfdcc1753d66e4f91e
DismissedLight <1686188646@qq.com>
2025-10-09 10:44:56 +0800

    impl #3147

--- src/Snap.Hutao/Snap.Hutao/Service/Game/Account/RegistryAccountDataListDecoder.cs
+++ src/Snap.Hutao/Snap.Hutao/Service/Game/Account/RegistryAccountDataListDecoder.cs
@@ -9,7 +9,7 @@ namespace Snap.Hutao.Service.Game.Account;
 
 internal static class RegistryAccountDataListDecoder
 {
-    private static ReadOnlySpan<byte> InitialVector { get => [0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF]; }
+    private static ReadOnlySpan<byte> InitializationVector { get => [0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF]; }
 
     public static string GetMacAddress()
     {
@@ -35,7 +35,7 @@ internal static class RegistryAccountDataListDecoder
         {
             string key = string.IsNullOrEmpty(address) || address.Length < 8 ? "FFFFFFFFFFFF" : address[..8];
             des.Key = Encoding.UTF8.GetBytes(key);
-            return Encoding.UTF8.GetString(des.DecryptCbc(Convert.FromBase64String(data), InitialVector));
+            return Encoding.UTF8.GetString(des.DecryptCbc(Convert.FromBase64String(data), InitializationVector));
         }
     }
 }
